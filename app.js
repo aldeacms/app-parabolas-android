b581d5f899b305cddc3492057227b9f9415684ad
Daniel Aldea <daniel@aldeacms.com>
2013-10-18 16:34:31 -0300

    file:///

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function onDeviceReady() {
 		$(this).parent().addClass("active");
 		
 		tab = $(this).attr("rel");
-		page = "res/pages/"+tab+".html";
+		page = "file:///pages/"+tab+".html";
 		titulo = $(this).attr("title");
 		
 		if (tab=="random"){
@@ -21,7 +21,7 @@ function onDeviceReady() {
 			else{
 				random = random+".html";	
 			}
-			URL = "res/pages/parabola"+random;
+			URL = "file:///pages/parabola"+random;
 			$.ajax({
 				type: 'GET',
 				url: URL,
@@ -55,7 +55,7 @@ function onDeviceReady() {
 					$("#listadoParabolas a").on("click",function(){
 						$(this).parent().parent().addClass("active");
 
-						URL = "res/pages/"+$(this).attr("href");
+						URL = "file:///pages/"+$(this).attr("href");
 						$.ajax({
 							type: 'GET',
 							url: URL,
