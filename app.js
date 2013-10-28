d5669eba419c63b67c0e8c3a9fc0d68efbc94e6d
Daniel Aldea <daniel@aldeacms.com>
2013-10-27 22:30:17 -0300

    test js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 });
 function onDeviceReady() {
 //$(document).ready(function(){
-	//navigator.splashscreen.hide();
+	navigator.splashscreen.hide();
 
 	$("#menu a").on("click",function(){
 		$("#menu td").removeClass("active");
@@ -12,7 +12,7 @@ function onDeviceReady() {
 		tab = $(this).attr("rel");
 		page = "pages/"+tab+".html";
 		titulo = $(this).attr("title");
-		URL = page;
+
 	
 		if (tab=="random"){
 			$("#btnSearch").hide();			
@@ -24,81 +24,102 @@ function onDeviceReady() {
 			else{
 				random = random+".html";	
 			}
-			titulo ="Par&aacute;bola Aleatoria";
 			URL = "pages/parabola"+random;
+			alert(URL);
+			$.ajax({
+				type: 'GET',
+				url: URL,
+				async:false,
+				dataType:'html',
+				success: function(data) { 
+
+					$("#content").html(data);
+					$("#content").scrollTop(0);
+					$("#header .titulo span").html("Par&aacute;bola Aleatoria");
+					return false;
+				}
+			});
 		}
-		else if(tab=="home"){
-			$("#btnBack").hide();
-			$("#btnSearch").show();
-		}
-
-		$.ajax({
-			type: 'GET',
-			url: URL,
-			async:false,
-			dataType:'html',
-			success: function(data) { 
-
-				$("#content").html(data);
-				$("#content").scrollTop(0);
-				$("#header .titulo span").html(titulo);
-
-				$("#listadoParabolas a").on("click",function(){
-					$(this).parent().parent().addClass("active");
-
-					URL = "pages/"+$(this).attr("href");
-					alert(URL);
-					$.ajax({
-						type: 'GET',
-						url: URL,
-						async:false,
-						dataType:'html',
-						success: function(data) { 
-							$("#btnBack").show();
-							$("#btnSearch").hide();
-
-							$("#content").html(data);
-							$("#content").scrollTop(0);
-							$("#header .titulo span").html("Lectura B&iacute;blica");
-							return false;
-						}
-					});
-				});	
-
-				$("#busqueda").on("keyup",function(){
-					var search_string = $("#busqueda").val();
+		else{
+			
+			alert(page);
+			$.ajax({
+				type: 'GET',
+				url: page,
+				async:false,
+				dataType:'html',
+				success: function(data) { 
+					$("#content").html(data);
+					$("#content").scrollTop(0);
+					$("#header .titulo span").html(titulo);
 					
-					search_string = search_string.toLowerCase();
+					if(tab=='home'){
+						$("#btnBack").hide();
+						$("#btnSearch").show();
 
-					$( ".parabola" ).each(function( index ) {
-						nombre = $(this).data("nombre");
-						if(nombre.search(search_string) !=-1){
-							$(this).parent().parent().parent().show();
-						}
-						else{
-							$(this).parent().parent().parent().hide();
-						}
+						$("#listadoParabolas a").on("click",function(){
+							$(this).parent().parent().addClass("active");
+
+							URL = "pages/"+$(this).attr("href");
+							alert(URL);
+							$.ajax({
+								type: 'GET',
+								url: URL,
+								async:false,
+								dataType:'html',
+								success: function(data) { 
+									$("#btnBack").show();
+									$("#btnSearch").hide();
+
+									$("#content").html(data);
+									$("#content").scrollTop(0);
+									$("#header .titulo span").html("Lectura B&iacute;blica");
+									return false;
+								}
+							});
+						});	
 						
+						$("#busqueda").on("keyup",function(){
+							var search_string = $("#busqueda").val();
+							
+							search_string = search_string.toLowerCase();
+
+							$( ".parabola" ).each(function( index ) {
+								nombre = $(this).data("nombre");
+								if(nombre.search(search_string) !=-1){
+									$(this).parent().parent().parent().show();
+								}
+								else{
+									$(this).parent().parent().parent().hide();
+								}
+								
+							});
+							
+						});
+						$("#btnClose").on("click",function(){
+							$("#busqueda").val("");
+							$("#busqueda").keyup();
+							return false;
+						});
+					}
+					else{
+						$("#btnBack").hide();
+						$("#btnSearch").hide();
+					}
+					
+					$("a[target=_blank]").on("click",function(){
+						URL = $(this).attr("href");
+						window.open(encodeURI(URL), '_blank','location=yes,closebuttoncaption=Volver');
+						return false;
 					});
 					
-				});
-
-				$("#btnClose").on("click",function(){
-					$("#busqueda").val("");
-					$("#busqueda").keyup();
 					return false;
-				});
-
-				$("a[target=_blank]").on("click",function(){
-					URL = $(this).attr("href");
-					window.open(encodeURI(URL), '_blank','location=yes,closebuttoncaption=Volver');
-					return false;
-				});
-
-				return false;
-			}
-		});
-
+				}
+			});
+		}
+		//$("#menu").trigger("close");
+				
+	});
 	
 	$("#btnBack").on("click",function(){
 		$("#tabHome").click();
