5216486fa953b4fefb0caf80446927c4a0c760c8
Daniel Aldea <daniel@aldeacms.com>
2013-10-27 22:44:20 -0300

    test JS

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ function onDeviceReady() {
 				random = random+".html";	
 			}
 			URL = "pages/parabola"+random;
-			alert(URL);
 			$.ajax({
 				type: 'GET',
 				url: URL,
@@ -41,8 +40,6 @@ function onDeviceReady() {
 			});
 		}
 		else{
-			
-			alert(page);
 			$.ajax({
 				type: 'GET',
 				url: page,
@@ -57,62 +54,64 @@ function onDeviceReady() {
 						$("#btnBack").hide();
 						$("#btnSearch").show();
 
-						$("#listadoParabolas a").on("click",function(){
-							$(this).parent().parent().addClass("active");
+						if($("#listadoParabolas").length){
+							$("#listadoParabolas a").on("click",function(){
+								$(this).parent().parent().addClass("active");
 
-							URL = "pages/"+$(this).attr("href");
-							alert(URL);
-							$.ajax({
-								type: 'GET',
-								url: URL,
-								async:false,
-								dataType:'html',
-								success: function(data) { 
-									$("#btnBack").show();
-									$("#btnSearch").hide();
+								URL = "pages/"+$(this).attr("href");
+								alert(URL);
+								$.ajax({
+									type: 'GET',
+									url: URL,
+									async:false,
+									dataType:'html',
+									success: function(data) { 
+										$("#btnBack").show();
+										$("#btnSearch").hide();
 
-									$("#content").html(data);
-									$("#content").scrollTop(0);
-									$("#header .titulo span").html("Lectura B&iacute;blica");
-									return false;
-								}
-							});
-						});	
+										$("#content").html(data);
+										$("#content").scrollTop(0);
+										$("#header .titulo span").html("Lectura B&iacute;blica");
+									}
+								});
+							});	
+						}
 						
-						// $("#busqueda").on("keyup",function(){
-						// 	var search_string = $("#busqueda").val();
+						
+						$("#busqueda").on("keyup",function(){
+							var search_string = $("#busqueda").val();
 							
-						// 	search_string = search_string.toLowerCase();
+							search_string = search_string.toLowerCase();
 
-						// 	$( ".parabola" ).each(function( index ) {
-						// 		nombre = $(this).data("nombre");
-						// 		if(nombre.search(search_string) !=-1){
-						// 			$(this).parent().parent().parent().show();
-						// 		}
-						// 		else{
-						// 			$(this).parent().parent().parent().hide();
-						// 		}
+							$( ".parabola" ).each(function( index ) {
+								nombre = $(this).data("nombre");
+								if(nombre.search(search_string) !=-1){
+									$(this).parent().parent().parent().show();
+								}
+								else{
+									$(this).parent().parent().parent().hide();
+								}
 								
-						// 	});
+							});
 							
-						// });
-						// $("#btnClose").on("click",function(){
-						// 	$("#busqueda").val("");
-						// 	$("#busqueda").keyup();
-						// 	return false;
-						// });
+						});
+						$("#btnClose").on("click",function(){
+							$("#busqueda").val("");
+							$("#busqueda").keyup();
+							return false;
+						});
 
-						// $("#btnSearch").on("click",function(){
-						// 	if($("#divBusqueda").is(":visible")){
-						// 		$("#divBusqueda").hide();
-						// 		$("#busqueda").val("");
-						// 		$("#busqueda").keyup();
-						// 	}
-						// 	else{
-						// 		$("#divBusqueda").show();
-						// 	}		
+						$("#btnSearch").on("click",function(){
+							if($("#divBusqueda").is(":visible")){
+								$("#divBusqueda").hide();
+								$("#busqueda").val("");
+								$("#busqueda").keyup();
+							}
+							else{
+								$("#divBusqueda").show();
+							}		
 							
-						// });	
+						});	
 					}
 					else{
 						$("#btnBack").hide();
